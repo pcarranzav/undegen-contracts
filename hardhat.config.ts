1dfc56b3e431bce5506673de2c0695927b98966a
Pablo Carranza Velez <pablo@edgeandnode.com>
2024-10-19 10:17:04 -0700

    chore: some scaffolding

---
+++ hardhat.config.ts
@@ -0,0 +1,51 @@
+import '@nomicfoundation/hardhat-foundry'
+import '@nomicfoundation/hardhat-toolbox'
+import '@nomicfoundation/hardhat-ignition-ethers'
+
+import { HardhatUserConfig } from 'hardhat/config'
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: '0.8.13',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  paths: {
+    artifacts: './build/contracts',
+    sources: './src',
+  },
+  networks: {
+    hardhat: {
+      accounts: {
+        mnemonic: 'myth like bonus scare over problem client lizard pioneer submit female collect',
+      },
+    },
+    unichainSepolia: {
+      url: 'https://sepolia.unichain.org',
+      accounts: {
+        mnemonic: process.env.MNEMONIC ?? '',
+      },
+    },
+  },
+  etherscan: {
+    apiKey: {
+      arbitrumSepolia: process.env.ETHERSCAN_API_KEY ?? '',
+    },
+    customChains: [
+      {
+        network: 'arbitrumSepolia',
+        chainId: 421614,
+        urls: {
+          apiURL: 'https://api-sepolia.arbiscan.io/api',
+          browserURL: 'https://sepolia.arbiscan.io/',
+        },
+      },
+    ],
+  },
+}
+
+export default config
