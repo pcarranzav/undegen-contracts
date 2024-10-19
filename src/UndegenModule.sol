80af91159410379b1075d008d5f8da06962c9f40
Pablo Carranza Velez <pablo@edgeandnode.com>
2024-10-19 14:18:42 -0700

    chore: giving it more shape

--- src/UndegenModule.sol
+++ src/UndegenModule.sol
@@ -1,15 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.27;
 
-import {ISafe} from "./safe/ISafe.sol";
+import {ISafe} from "./interfaces/ISafe.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {IHyperdrive} from "hyperdrive/contracts/src/interfaces/IHyperdrive.sol";
 import {IHyperdriveCore} from "hyperdrive/contracts/src/interfaces/IHyperdriveCore.sol";
 
 contract UndegenModule {
-    
-    struct Asset {
-        address token;
-        uint256 usdAmount;
-    }
+
+    error UndegenModuleBondNotMatured();
 
     uint256 bondMaturity;
     uint256 bondAmount;
@@ -23,9 +22,29 @@ contract UndegenModule {
         riskyAssets = _riskyAssets;
     }
 
-    function rebalance(address _safe, Asset[] memory _riskyAssets) public {
+    function rebalance(address _safe, uint[] memory _riskyAssetUSDAmounts) public {
         ISafe safe = ISafe(_safe);
 
+        // If we have a bond on Hyperdrive, it must have matured to rebalance
+        // TODO: In the future we could rebalance anyways as long as closing
+        // the long would have a positive return.
+        require(bondMaturity < block.timestamp, UndegenModuleBondNotMatured());
+        IHyperdrive.Options memory hyperdriveOpts = IHyperdrive.Options({
+            destination: _safe,
+            asBase: false,
+            extraData: ""
+        });
+        _execOnSafe(
+            safe,
+            address(hyperdrivePool),
+            0,
+            abi.encodeCall(hyperdrivePool.closeLong, (bondMaturity, bondAmount, 0, hyperdriveOpts))
+        );
+    }
 
+    function _execOnSafe(ISafe _safe, address _to, uint256 _value, bytes memory _data) internal returns (bytes memory) {
+        (bool success, bytes memory data) = _safe.execTransactionFromModuleReturnData(_to, _value, _data, 0);
+        require(success, string(data));
+        return data;
     }
 }
--- src/interfaces/ISafe.sol
+++ src/interfaces/ISafe.sol
@@ -3,4 +3,10 @@ pragma solidity ^0.8.27;
 
 interface ISafe {
     function execTransactionFromModule(address to, uint256 value, bytes memory data, uint8 operation) external returns (bool success);
+    function execTransactionFromModuleReturnData(
+        address to,
+        uint256 value,
+        bytes memory data,
+        uint8 operation
+    ) external returns (bool success, bytes memory returnData);
 }
