77cf98024b77671b9b62c4531028ae0e0cb38100
Pablo Carranza Velez <pablo@edgeandnode.com>
2024-10-19 12:39:08 -0700

    chore: adding safe module

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -6,7 +6,7 @@ import { HardhatUserConfig } from 'hardhat/config'
 
 const config: HardhatUserConfig = {
   solidity: {
-    version: '0.8.13',
+    version: '0.8.27',
     settings: {
       optimizer: {
         enabled: true,
--- src/UndegenModule.sol
+++ src/UndegenModule.sol
@@ -1,6 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
+import { IModuleManager } from "./safe/IModuleManager.sol";
+
 contract UndegenModule {
     
+    struct Asset {
+        address token;
+        uint256 dollarAmount;
+    }
+    function rebalance(address _safe, Asset[] memory _assets) public {
+        
+    }
 }
